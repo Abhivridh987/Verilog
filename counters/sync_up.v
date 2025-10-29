0b6f14be2258cf1a94a0249f0b54d1dd5ad29b0c
Abhivridh <abhivridh2@gmail.com>
2025-10-29 19:54:40 +0530

    Add synchronous up counter and testbench module

---
+++ counters/sync_up.v
@@ -0,0 +1,37 @@
+module sync_up_counter(clk, reset, Q);
+    input clk, reset;
+    output reg [2:0] Q;
+    
+    always @(posedge clk)
+    begin
+        if(reset == 1)
+            Q <= 3'b000;
+        else
+            Q <= Q + 1;
+    end
+endmodule
+
+module testing();
+    reg clk, reset;
+    wire [2:0] Q;
+    
+    async_up_counter g(clk, reset, Q);
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+    initial begin
+        $dumpfile("testing.vcd");
+        $dumpvars(1, testing);
+        $monitor("Time : %0dns  %b%b%b", $time, Q[2], Q[1], Q[0]);
+        
+        reset = 1;
+        #10;
+        reset = 0;
+        
+        #100;
+        $finish;
+    end
+endmodule
+        
+    
